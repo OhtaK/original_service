92b984eab1c682c33922c09a8663ae65d1fdcc0c
Keisuke Ohta <dadadak315@gmail.com>
2017-03-30 05:39:15 +0000

    make Word db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+word1  = Word.create(eng: "access", jap: "アクセス")
+word2  = Word.create(eng: "afford", jap: "余裕がある")
+word3  = Word.create(eng: "amount", jap: "量")
+word4  = Word.create(eng: "approximate", jap: "おおよそ")
+word5  = Word.create(eng: "arrange", jap: "手配する")
+word6  = Word.create(eng: "bill", jap: "請求書")
+word7  = Word.create(eng: "book", jap: "予約する")
+word8  = Word.create(eng: "budget", jap: "予算")
+word9  = Word.create(eng: "charge", jap: "請求する")
+word10  = Word.create(eng: "check", jap: "確認する")
