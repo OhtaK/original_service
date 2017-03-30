dd46a0ff59fbc71220fbfe7ea67a9f8f7633149f
Keisuke Ohta <dadadak315@gmail.com>
2017-03-30 03:57:41 +0000

    make transition to question page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root 'top#index'
+  
+  get 'query',  to: 'question#query'
+  
+  resources :question
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
